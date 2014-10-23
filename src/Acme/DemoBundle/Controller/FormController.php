024f8324e8d130cc0d62ae28ccb451d07b3846ac
Cosmin-Mihai Tutunaru <ctutunaru@pitechplus.com>
2014-10-23 09:45:05 +0300

    Add the FormController in order to test the forms.

---
+++ src/Acme/DemoBundle/Controller/FormController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Acme\DemoBundle\Model\A;
+use Acme\DemoBundle\Model\B;
+use Acme\DemoBundle\Form\AType;
+
+/**
+ * Class: FormController
+ *
+ * @see Controller
+ */
+class FormController extends Controller
+{
+    /**
+     * @Route("/nested-forms", name="nested_forms")
+     * @Template()
+     */
+    public function nestedFormsAction(Request $request)
+    {
+
+        $a = new A();
+
+        $b = new B();
+        $a->getBs()->add($b);
+
+        $b = new B();
+        $a->getBs()->add($b);
+
+        $form = $this->createForm(new AType(), $a);
+        $form->handleRequest($request);
+
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+}
